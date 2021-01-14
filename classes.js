c9d1bde3192f838a844deedd880c8283233bb174
Joel Hernandez <jhernandez98@outlook.es>
2021-01-14 09:29:08 -0500

    Add files via upload

--- classes.js
+++ classes.js
@@ -10,9 +10,7 @@ export class UIBookList {
   
             if (e.target.matches("input")) {
                 e.target.classList.add("focus-effect")
-            }
-
-           
+            } 
         }
 
         else {
@@ -24,14 +22,57 @@ export class UIBookList {
 
     validateInputFields = (args) =>
     {
-       
+        let result = false;
 
         args.forEach((input) => { 
             if (input.value === "")
             {
-                alert();
+                result = true;
+                
             }
         })
+        return result;
     }
 
+   
+    emptyFieldMessageAlert = () =>{
+       
+        const messageContainer = document.querySelector(".message-container");
+       const alert = document.createElement("div");
+       const text = document.createElement("p");
+
+       text.textContent = "Please fill out all Empty Fields";
+       text.classList.add(".message");
+       alert.classList.add("alert");
+       alert.appendChild(text);
+
+        
+       messageContainer.style.display = "flex";
+       messageContainer.appendChild(alert);
+
+    }
+
+    removeMessageAlert = () =>{
+       document.querySelector(".message-container").style.display = "none";  
+    }
+
+}
+
+
+export class Book{
+   
+    constructor(){
+       
+    }
+    
+    addBook = (...args) =>{
+     
+       const [title] = [...args]; // destructuring the array
+        const objBook = {...args}; // creating an object out of the array
+       
+      
+       localStorage.setItem(title,JSON.stringify(objBook));
+      console.log( JSON.parse(localStorage.getItem(title)));
+        
+    }
 }
--- main.js
+++ main.js
@@ -1,17 +1,28 @@
 
-import { UIBookList } from "./classes.js";
+import { UIBookList,Book } from "./classes.js";
 
 const formContainer = document.querySelector(".form-container");
 const btnSubmit = document.querySelector("#btn");
 
 const forminput = new UIBookList();
+const book = new Book();
 
 
 
 
 const validateFields = () => {
-  console.log("hello")
-  forminput.validateInputFields(Array.from(document.querySelectorAll("input")));
+ 
+  if(!forminput.validateInputFields(Array.from(document.querySelectorAll("input"))))
+  {
+     forminput.removeMessageAlert();
+     book.addBook(document.getElementById("title").value,document.getElementById("Author").value,document.getElementById("isbn").value)
+
+  }
+  else{
+    
+     if(document.querySelector(".message-container").childElementCount == 0)
+    forminput.emptyFieldMessageAlert();
+  }
 }
 
 formContainer.addEventListener("focusin", forminput.getFocus);
